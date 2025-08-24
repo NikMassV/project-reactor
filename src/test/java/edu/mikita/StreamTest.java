2b6126fad48c642aa7d3e2c8b1348d9c3e46cf05
slipk <slipknoshka777@gmail.com>
2025-08-24 23:28:55 +0200

    add various async stream examples

--- src/test/java/edu/mikita/StreamTest.java
+++ src/test/java/edu/mikita/StreamTest.java
@@ -2,9 +2,11 @@ package edu.mikita;
 
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
 import java.util.Arrays;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
@@ -55,7 +57,7 @@ public class StreamTest {
     }
 
     @Test
-    public void asyncStream() {
+    public void asyncStream_subscribeOn_withoutAwait() {
         Flux.<Integer>create(s -> {
                     s.next(50);
                     s.next(50);
@@ -78,6 +80,138 @@ public class StreamTest {
                         .map(i -> "Number " + i + " is prime: " + checker.test(i)))
                 .subscribeOn(Schedulers.boundedElastic())
                 .subscribe(result -> System.out.println("IN thread [" + Thread.currentThread().getName() + "] -> " + result));
+    }
+
+    @Test
+    public void asyncStream_subscribeOn_withAwait() throws InterruptedException {
+        var cdl = new CountDownLatch(1);
+        Flux.<Integer>create(s -> {
+                    s.next(50);
+                    s.next(50);
+                    s.next(50);
+                    s.next(50);
+                    s.next(100);
+                    s.next(100);
+                    s.next(100);
+                    s.next(100);
+                    s.next(150);
+                    s.next(150);
+                    s.next(150);
+                    s.next(150);
+                    s.complete();
+                }).map(this::generateIntArray)
+                .map(array -> Arrays.stream(array)
+                        .filter(i -> (i % 2 != 0))
+                        .collect(Collectors.toList()))
+                .flatMap(list -> Flux.fromIterable(list)
+                        .map(i -> "Number " + i + " is prime: " + checker.test(i)))
+                .subscribeOn(Schedulers.boundedElastic())
+                .doFinally(ignore -> cdl.countDown())
+                .subscribe(result -> System.out.println("IN thread [" + Thread.currentThread().getName() + "] -> " + result));
+        cdl.await();
+    }
+
+    @Test
+    public void asyncStream_publishOn_withAwait() throws InterruptedException {
+        var cdl = new CountDownLatch(1);
+
+        Flux.<Integer>create(s -> {
+                    s.next(50);
+                    s.next(50);
+                    s.next(50);
+                    s.next(50);
+                    s.next(100);
+                    s.next(100);
+                    s.next(100);
+                    s.next(100);
+                    s.next(150);
+                    s.next(150);
+                    s.next(150);
+                    s.next(150);
+                    s.complete();
+                }).map(this::generateIntArray)
+                .map(array -> {
+                    System.out.println("IN thread [" + Thread.currentThread().getName() + "] -> filtering array with size: " + array.length);
+                    return Arrays.stream(array)
+                            .filter(i -> (i % 2 != 0))
+                            .collect(Collectors.toList());
+                })
+                .publishOn(Schedulers.boundedElastic())
+                .flatMap(list -> Flux.fromIterable(list)
+                        .map(i -> "Number " + i + " is prime: " + checker.test(i)))
+                .doFinally(ignore -> cdl.countDown())
+                .subscribe(result -> System.out.println("IN thread [" + Thread.currentThread().getName() + "] -> " + result));
+
+        cdl.await();
+    }
+
+    @Test
+    public void researchReactor_publishOn_innerFlux() throws InterruptedException {
+        var cdl = new CountDownLatch(1);
+
+        Flux.<Integer>create(s -> {
+                    s.next(50);
+                    s.next(50);
+                    s.next(50);
+                    s.next(50);
+                    s.next(100);
+                    s.next(100);
+                    s.next(100);
+                    s.next(100);
+                    s.next(150);
+                    s.next(150);
+                    s.next(150);
+                    s.next(150);
+                    s.complete();
+                }).map(this::generateIntArray)
+                .map(array -> {
+                    System.out.println("IN thread [" + Thread.currentThread().getName() + "] -> filtering array with size: " + array.length);
+                    return Arrays.stream(array)
+                            .filter(i -> (i % 2 != 0))
+                            .collect(Collectors.toList());
+                })
+                .flatMap(list -> Flux.fromIterable(list)
+                        .publishOn(Schedulers.boundedElastic())
+                        .map(i -> "Number " + i + " is prime: " + checker.test(i)))
+                .doFinally(ignore -> cdl.countDown())
+                .subscribe(result -> System.out.println("IN thread [" + Thread.currentThread().getName() + "] -> " + result));
+
+        cdl.await();
+    }
+
+    @Test
+    public void researchReactor_subscribeOn_perElement() throws InterruptedException {
+        var cdl = new CountDownLatch(1);
+
+        Flux.<Integer>create(s -> {
+                    s.next(50);
+                    s.next(50);
+                    s.next(50);
+                    s.next(50);
+                    s.next(100);
+                    s.next(100);
+                    s.next(100);
+                    s.next(100);
+                    s.next(150);
+                    s.next(150);
+                    s.next(150);
+                    s.next(150);
+                    s.complete();
+                }).map(this::generateIntArray)
+                .map(array -> {
+                    System.out.println("IN thread [" + Thread.currentThread().getName() + "] -> filtering array with size: " + array.length);
+                    return Arrays.stream(array)
+                            .filter(i -> (i % 2 != 0))
+                            .collect(Collectors.toList());
+                })
+                .flatMap(list -> Flux.fromIterable(list)
+                        .flatMap(i -> Mono.defer(() -> Mono.just("Number " + i + " is prime: " + checker.test(i)))
+                                .subscribeOn(Schedulers.boundedElastic()))
+                )
+                .doFinally(ignore -> cdl.countDown())
+                .subscribe(result -> System.out.println("IN thread [" + Thread.currentThread().getName() + "] -> " + result));
+
+        cdl.await();
     }
 
     private Integer[] generateIntArray(int size) {
